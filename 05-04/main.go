712c0a2df0feba62fa8d1bbe4df6fc5f52f07c85
mingyong365 <mingyong@email.szu.edu.cn>
2020-05-13 22:22:00 +0800

    add 05.04 problem

--- 05-04/main.go
+++ 05-04/main.go
@@ -1,1 +1,19 @@
-package _5_04
+package main
+
+import "fmt"
+
+func main() {
+	var val = 0x7fffffff + 2
+	fmt.Println(val)
+}
+
+//func findClosedNumbers(num int) []int {
+//	return nil
+//}
+//
+//func swapBit(x, i, j int) int {
+//	if ((x >> i) & 1) != ((x >> j) & 1) {
+//		x ^= (1 << i) | (1 << j)
+//	}
+//	return x
+//}
