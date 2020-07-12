4f728a98475e922e3a11f2156021b06955a0a6c5
mingyong365 <mingyong@email.szu.edu.cn>
2020-07-12 13:24:16 +0800

    add 52 problem

---
+++ JZOffer/52/main.go
@@ -0,0 +1,37 @@
+package main
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	listACount := listNodeCount(headA)
+	listBCount := listNodeCount(headB)
+
+	if listACount > listBCount {
+		diff := listACount - listBCount
+		for i := 0; i < diff; i++ {
+			headA = headA.Next
+		}
+	} else {
+		diff := listBCount - listACount
+		for i := 0; i < diff; i++ {
+			headB = headB.Next
+		}
+	}
+
+	for headA != nil && headB != nil {
+		if headA == headB {
+			return headA
+		}
+		headA = headA.Next
+		headB = headB.Next
+	}
+
+	return nil
+}
+
+func listNodeCount(head *ListNode) int {
+	var count int
+	for head != nil {
+		count++
+		head = head.Next
+	}
+	return count
+}
